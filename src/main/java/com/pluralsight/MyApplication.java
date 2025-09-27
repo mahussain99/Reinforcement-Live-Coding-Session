18133aa285cd0ff4cf2d682597fab577cf050fad
Family <mh5212357@gmail.com>
2025-09-27 11:18:09 -0400

    I am working Reinforcement-lab I am done from 1 to 10

--- src/main/java/com/pluralsight/MyApplication.java
+++ src/main/java/com/pluralsight/MyApplication.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class MyApplication {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner myscanner = new Scanner(System.in);
 
         System.out.println("What is your name?");
-        String name = scanner.nextLine();
+        String name = myscanner.nextLine();
         System.out.println("What is your age?");
-        int age = scanner.nextInt();
+        int age = myscanner.nextInt();
 
         //Step 4 - Using the scanner with if statements
 
@@ -35,10 +35,19 @@ public class MyApplication {
             System.out.println("=================================");
             NameAndAge(name, age);
 
+            //Step 9 - Using the Math library
+            System.out.println("==================================");
+            double fromNumber = getNumber(myscanner);
+            double Sqrt = Calculate_Sqr(fromNumber);
+
+            System.out.println("==================================");
+            //  //Step 10 - Using a switch statement
+
+            printWelcomeMessage(name);
         }
     }
 
-//Step 5 - Declaring methods and calling them
+    //Step 5 - Declaring methods and calling them
 
     public static void Personalized_message(String name, int age) {
         System.out.println("Hello my name is " + name + " I am " + age + " years old");
@@ -52,7 +61,7 @@ public class MyApplication {
             System.out.println("They are not old enough to vote.");
         }
     }
-//Step 7 - Comparing Strings
+    //Step 7 - Comparing Strings
 
     public static void print_message(String name) {
         if (name.equalsIgnoreCase("Alice")) {
@@ -70,6 +79,31 @@ public class MyApplication {
             System.out.println("they are not old enough to drink. ");
         }
 
+    }
+    //Step 9 - Using the Math library
+
+    public static double getNumber(Scanner Scanner) {
+        System.out.println("Enter number");
+        return Scanner.nextDouble();
+    }
+
+    public static double Calculate_Sqr(double number) {
+        return Math.sqrt(number);
 
     }
+
+    //Step 10 - Using a switch statement
+    public static void printWelcomeMessage(String name) {
+        if (name.equalsIgnoreCase("Alice"))
+            System.out.println("message welcoming her");
+        else if (name.equalsIgnoreCase("Bob"))
+            System.out.println("asking him if he wants to grab a drink.");
+        else {
+            System.out.println("Your name not in the list");
+
+        }
+    }
 }
+
+
+
